14bf90651bffc764845f6b67676a65ccb4fa2700
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-12-29 16:44:59 +0530

    code ready to begin; sisues resolved

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-/**
- * Created by tanmay.rajani on 29/12/15.
- */
-
-'use strict';
-
 var myApp = angular.module('myApp',[]);
-
 myApp.factory('Data',function () {
     return {message: ""}
 });
 
 myApp.filter('play',function (Data) {
     return function (text) {
-        text = text.substring(text.lastIndexOf("\n")+1);
-        var index = text.indexOf(':');
-        if(index>-1){
-            var first = text.substring(0,index);
-            var last = text.substring(index+1);
-            $(".portion-ri").css(first,last);
-        }
+        // code goes here
         return text;
     }
 })
-
 
 function FirstCtrl($scope,Data){
     $scope.data = Data;
