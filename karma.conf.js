0831c3e8d26ee9b932b5e85c77ae0bee6f97519d
Tanmay Rajani <rajani.tanmay@gmail.com>
2016-02-01 13:57:58 +0530

    finallygit statusgit statusgit status! karma working with bower

--- karma.conf.js
+++ karma.conf.js
@@ -15,10 +15,12 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-	    'etc/lib/angular.min.js',
-	    'etc/lib/angular-*.js',
-	    'bower_components/**/*.js',
-	    'etc/**/*.js'
+	    "bower_components/jquery/dist/jquery.min.js",
+	    "bower_components/angular/angular.min.js",
+	    "bower_components/angular-mocks/angular-mocks.js",
+	    "bower_components/sinonjs/sinon.js",
+	    "etc/*.js",
+	    "etc/test/*.js"
     ],
 
 
